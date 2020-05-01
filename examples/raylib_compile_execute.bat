a5a25fdd132b673a6689610c7235697931a02341
Tomas Orti <tomas.orti.martin@ericsson.com>
2020-05-01 23:46:50 +0200

    Tested examples with mingw-w64 64 bits on Windows 10

--- examples/raylib_compile_execute.bat
+++ examples/raylib_compile_execute.bat
@@ -1,14 +1,18 @@
-::@echo off
+@echo off
 :: .
 :: Compile your examples using:  raylib_compile_execute.bat core/core_basic_window.c
 :: .
 :: > Setup required Environment
 :: -------------------------------------
-set RAYLIB_INCLUDE_DIR=C:\GitHub\raylib\src
-set RAYLIB_LIB_DIR=C:\GitHub\raylib\src
-set RAYLIB_RES_FILE=C:\GitHub\raylib\raylib.rc.data
-set COMPILER_DIR=C:\raylib\mingw\bin
-set PATH=%PATH%;%COMPILER_DIR%
+set RAYLIB_SRC=C:\raylib\src
+
+set RAYLIB_INCLUDE_DIR=%RAYLIB_SRC%
+set RAYLIB_LIB_DIR=%RAYLIB_SRC%
+set RAYLIB_RES_FILE=%RAYLIB_SRC%\raylib.rc.data
+
+::set COMPILER_DIR=C:\raylib\mingw\bin
+::set PATH=%PATH%;%COMPILER_DIR%
+
 :: Get full filename path for input file %1
 set FILENAME=%~f1
 set NAMEPART=%FILENAME:~0,-2%
@@ -17,6 +21,8 @@ cd %~dp0
 :: > Cleaning latest build
 :: ---------------------------
 cmd /c if exist %NAMEPART%.exe del /F %NAMEPART%.exe
+
+@echo on
 :: .
 :: > Compiling program
 :: --------------------------
@@ -25,8 +31,10 @@ cmd /c if exist %NAMEPART%.exe del /F %NAMEPART%.exe
 :: -std=c99  : Use C99 language standard
 :: -Wall     : Enable all compilation Warnings
 :: -mwindows : Compile a Windows executable, no cmd window
-gcc -o %NAMEPART%.exe %FILENAME% %RAYLIB_RES_FILE% -s -O2 -I%RAYLIB_INCLUDE_DIR% -I%RAYLIB_LIB_DIR% -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows
+gcc -o %NAMEPART%.exe %FILENAME% -s -O2 -I%RAYLIB_INCLUDE_DIR% -L%RAYLIB_LIB_DIR% -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows
 :: .
 :: > Executing program
 :: -------------------------
 cmd /c if exist %NAMEPART%.exe %NAMEPART%.exe
+
+@echo off
